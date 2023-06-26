c709495b833749f5e06c76faefd03012fa31ccbd
shy8818 <83875932+shyhwan@users.noreply.github.com>
2023-06-26 23:49:27 +0900

    프론트 js 파일 만들기

--- app/app.js
+++ app/app.js
@@ -11,6 +11,8 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`));
+app.use("/css", express.static(`${__dirname}/node_modules/bootstrap/dist/css`));
 app.use("/", home); // 미들웨어 등록하는 메소드
 
 module.exports = app;
