f0f7ab707acc49a184f25deff66bab864a2adf05
shy8818 <83875932+shyhwan@users.noreply.github.com>
2023-06-26 23:38:58 +0900

    폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -5,10 +5,10 @@ const express = require("express");
 const app = express();
 
 // 라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home); // 미들웨어 등록하는 메소드
