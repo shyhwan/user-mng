7d088ef3111693a0e9ad4c4592f3fa0ef4e11bcb
shy8818 <83875932+shyhwan@users.noreply.github.com>
2023-07-01 00:49:02 +0900

    login 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const id = document.querySelector("#id"),
+  pw = document.querySelector("#pw"),
+  loginBtn = document.querySelector("#btn");
+
+const login = (e) => {
+  e.preventDefault();
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+  fetch("/admin", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
+};
+
+loginBtn.addEventListener("click", login);
